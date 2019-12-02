275a3d5253026952359543cfb76e407e01699eb8
Richard Van Camp <richardvancamp@Richards-MacBook-Pro.local>
2019-12-02 16:44:26 +0200

    feat: add example store test

---
+++ src/store.test.js
@@ -0,0 +1,13 @@
+import { store } from './immer-actions-store.js';
+
+const { actions } = store
+
+test('example store test', () => {
+  let current = undefined
+  store.subscribe((val) => {
+    current = val
+  })
+  actions.inc()
+  actions.show()
+  expect(current).toEqual({ visible: true, dogs: 1 })
+})
